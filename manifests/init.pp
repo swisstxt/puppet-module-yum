b85c0fda07c140afc2a8865f4bfbe854e86f21d5
Marcel Haerry <haerry@puzzle.ch>
2008-04-21 16:51:13 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# yum module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "yum": }
+class yum {}
