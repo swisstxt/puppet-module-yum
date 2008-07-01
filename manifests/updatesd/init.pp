d3cce508c0258f75ca06d76d1a65f1e2a9b92ad1
Marcel Haerry <haerry@puzzle.ch>
2008-07-01 12:15:38 +0200

    fixed correct lookup path

--- manifests/updatesd/init.pp
+++ manifests/updatesd/init.pp
@@ -11,15 +11,3 @@ class yum::updatesd {
         require => Package['yum-updatesd'],
     }
 }
-
-class yum::updatesd::disable inherits yum::updatesd {
-    Package['yum-updatesd']{
-        ensure => absent,
-    }
-
-    Service['yum-updatesd']{
-        ensure => stopped,
-        enable => false,
-        require => undef,
-    }
-}
