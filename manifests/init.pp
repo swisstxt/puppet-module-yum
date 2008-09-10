75f1bd32341285559ef55d039fcb3cba5a0cadd2
Marcel Haerry <haerry@puzzle.ch>
2008-09-10 19:52:00 +0200

    added jpackage yum repo

--- manifests/init.pp
+++ manifests/init.pp
@@ -246,6 +246,34 @@ class yum::remi {
     }
 }
 
+class yum::jpackage {
+    yum::managed_yumrepo { 'jpackage-generic':
+        desc => 'JPackage (free), generic',
+        mirrorlist => 'http://www.jpackage.org/jpackage_generic_1.7.txt',
+        failovermethod => 'priority',
+        gpgcheck => 1,
+        gpgkey => 'http://www.jpackage.org/jpackage.asc',
+        enabled => 1,
+        priority => 1,
+    }
+    yum::managed_yumrepo { 'jpackage-rhel':
+        descr => 'JPackage (free) for Red Hat Enterprise Linux $releasever',
+        mirrorlist => 'http://www.jpackage.org/jpackage_rhel-$releasever_1.7.txt',
+        failovermethod => 'priority',
+        gpgcheck => 1,
+        gpgkey => 'http://www.jpackage.org/jpackage.asc',
+        enabled => 1,
+    }
+    yum::managed_yumrepo { 'jpackage-generic-nonfree':
+        descr => 'JPackage (non-free), generic',
+        mirrorlist => 'http://www.jpackage.org/jpackage_generic_nonfree_1.7.txt',
+        failovermethod => 'priority',
+        gpgcheck => 1,
+        gpgkey => 'http://www.jpackage.org/jpackage.asc',
+        enabled => 0,
+    }
+}
+
 class yum::prerequisites {
     package{yum-priorities:
         ensure => present,
