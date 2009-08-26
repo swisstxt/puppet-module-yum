21a771213bea62556b21b2b73d10c3208779cf69
Simon Josi <josi@puzzle.ch>
2009-08-26 18:23:20 +0200

    do it right please

--- manifests/centos/testing.pp
+++ manifests/centos/testing.pp
@@ -1,8 +1,10 @@
-yum::managed_yumrepo{testing:
-    descr => 'CentOS-$releasever - Testing',
-    baseurl => 'http://dev.centos.org/centos/$releasever/testing/$basearch',
-    enabled => 1,
-    gpgcheck => 1,
-    gpgkey => 'http://dev.centos.org/centos/RPM-GPG-KEY-CentOS-testing',
-    priority => 3,
-}
+class yum::centos::testing {
+    yum::managed_yumrepo{testing:
+        descr => 'CentOS-$releasever - Testing',
+        baseurl => 'http://dev.centos.org/centos/$releasever/testing/$basearch',
+        enabled => 1,
+        gpgcheck => 1,
+        gpgkey => 'http://dev.centos.org/centos/RPM-GPG-KEY-CentOS-testing',
+        priority => 3,
+    }
+T}
