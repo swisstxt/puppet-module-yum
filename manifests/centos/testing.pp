29eb99d443a8429b5e9b58eb5e851b09797e111a
Simon Josi <josi@puzzle.ch>
2009-09-01 15:03:34 +0200

    strip testing repo to a php-only repo
    we do this by including only relevant packages. also we
    have to set the priority to the same level as the base packages.
    rename the repo to testing-php for clarfication.

--- manifests/centos/testing.pp
+++ manifests/centos/testing.pp
@@ -1,10 +1,11 @@
 class yum::centos::testing {
-    yum::managed_yumrepo{testing:
+    yum::managed_yumrepo{testing-php:
         descr => 'CentOS-$releasever - Testing',
         baseurl => 'http://dev.centos.org/centos/$releasever/testing/$basearch',
         enabled => 1,
         gpgcheck => 1,
         gpgkey => 'http://dev.centos.org/centos/RPM-GPG-KEY-CentOS-testing',
-        priority => 3,
+        priority => 1,
+        includepkgs => [ 'php*', 'mysql*', 'postgresql*' ]
     }
 }
