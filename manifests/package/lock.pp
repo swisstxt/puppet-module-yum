af97d0dcffcceec9642c5d8a49c3414794aa01cb
Anselm Strauss <strauss@puzzle.ch>
2012-07-24 16:49:53 +0200

    fix: reversed order of augeas commands (other order fails, do not know reason)

--- manifests/package/lock.pp
+++ manifests/package/lock.pp
@@ -5,7 +5,8 @@ define yum::package::lock($version, $epoch = "") {
   augeas {
     "${name}_yum_versionlock":
       context => "/etc/yum/pluginconf.d/versionlock.list/$name",
-      changes => [ "set version $version", "set epoch $epoch" ];
+      # reverse order of commands fails, why?
+      changes => [ "set epoch $epoch", "set version $version" ];
   }
 
 }
