7421bdb89d2c6d85552a32b033ce0b59e15c8a6f
Anselm Strauss <strauss@puzzle.ch>
2012-07-26 10:43:41 +0200

    fix: do not set empty epoch

--- manifests/package/lock.pp
+++ manifests/package/lock.pp
@@ -2,11 +2,19 @@ define yum::package::lock($version, $epoch = "") {
 
   require yum::versionlock
 
+  $change_version = "set version $version"
+  $change_epoch = "set epoch $epoch"
+  if $epoch = '' {
+    $changes = [ $change_version ]
+  } else {
+    # reverse order of commands fails, why?
+    $changes = [ $change_epoch, $change_version ]
+  }
+
   augeas {
     "${name}_yum_versionlock":
       context => "/etc/yum/pluginconf.d/versionlock.list/$name",
-      # reverse order of commands fails, why?
-      changes => [ "set epoch $epoch", "set version $version" ];
+      changes => $changes;
   }
 
 }
