13afe5209d5fa873ff22a4d7dad584978ebe7f52
Anselm Strauss <strauss@puzzle.ch>
2013-01-10 16:22:00 +0100

    more puppet like implementation of versionlock with ensure parameter instead of lock/unlock defines

---
+++ manifests/package/versionlock.pp
@@ -0,0 +1,14 @@
+class yum::package::versionlock {
+
+  if $lsbmajdistrelease == '6' {
+    $package_name = 'yum-plugin-versionlock'
+  } else {
+    $package_name = 'yum-versionlock'
+  }
+
+  package {
+    $package_name:
+      ensure => present;
+  }
+  
+}
--- manifests/versionlock.pp
+++ manifests/versionlock.pp
@@ -1,14 +1,25 @@
-class yum::versionlock {
+define yum::versionlock($ensure, $epoch = '') {
 
-  if $lsbmajdistrelease == '6' {
-    $package_name = 'yum-plugin-versionlock'
+  require yum::package::versionlock
+
+  if $ensure == 'absent' {
+    $changes = [ "rm $name" ]
   } else {
-    $package_name = 'yum-versionlock'
+    if $epoch == '' {
+      $changes = [ "set ${name}/version $ensure" ]
+    } else {
+      # reverse order of commands fails, why?
+      $changes = [ "set ${name}/epoch $epoch", "set ${name}/version $ensure" ]
+    }
   }
 
-  package {
-    $package_name:
-      ensure => present;
+  augeas {
+    "${name}_yum_versionlock":
+      context   => '/files/etc/yum/pluginconf.d/versionlock.list',
+      incl      => '/etc/yum/pluginconf.d/versionlock.list',
+      lens      => 'YumVersionlock.lns',
+      load_path => '/var/lib/puppet/lib/augeas/lenses',
+      changes   => $changes;
   }
-  
+
 }
