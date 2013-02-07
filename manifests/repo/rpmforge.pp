6eadab87b486ed08d339c3ec3be276afe7187417
root <root@puppet03.stxt-cloud.local>
2013-02-07 11:16:48 +0100

    update rpmforge

--- manifests/repo/rpmforge.pp
+++ manifests/repo/rpmforge.pp
@@ -5,8 +5,19 @@ class yum::repo::rpmforge(
   Class['yum::repo::rpmforge'] <- Class['yum']
 
   yum::repo{'rpmforge':
-    descr => "RPMForge RHEL$releasever packages",
-    baseurl => 'http://wftp.tu-chemnitz.de/pub/linux/dag/redhat/el$releasever/en/$basearch/dag',
+    descr => "RPMForge RHEL$releasever packages - dag",
+    baseurl => 'http://apt.sw.be/redhat/el$releasever/en/$basearch/rpmforge',
+    mirrorlist => 'http://apt.sw.be/redhat/el$releasever/en/mirrors-rpmforge',
+    enabled => 1,
+    priority => $priority,
+    gpgcheck => 1,
+    includepkgs => $includepkgs,
+    gpgkey => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-rpmforge-dag',
+  }
+  yum::repo{'rpmforge-extras':
+    descr => "RPMForge RHEL$releasever packages - extras",
+    baseurl => 'http://apt.sw.be/redhat/el$releasever/en/$basearch/extras',
+    mirrorlist => 'http://apt.sw.be/redhat/el$releasever/en/mirrors-rpmforge-extras',
     enabled => 1,
     priority => $priority,
     gpgcheck => 1,
